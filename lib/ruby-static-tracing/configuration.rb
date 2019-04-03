d90564ae209e12af55f7404fc94a6e3576c60818
derekstride <djgstride@gmail.com>
2019-04-03 11:00:49 -0400

    Add config constants

--- lib/ruby-static-tracing/configuration.rb
+++ lib/ruby-static-tracing/configuration.rb
@@ -1,8 +1,20 @@
+# frozen_string_literal: true
+
 require 'singleton'
 
 module StaticTracing
   class Configuration
     include Singleton
     attr_accessor :mode, :signal
+
+    module Modes
+      ON = 'ON'
+      OFF = 'OFF'
+      SIGNAL = 'SIGNAL'
+
+      module SIGNALS
+        SIGPROF = 'PROF'
+      end
+    end
   end
 end
