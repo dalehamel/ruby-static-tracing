ea62f960d7503526849de7a28acede471de32e97
Dale Hamel <dale.hamel@srvthe.net>
2019-04-05 14:19:18 -0400

    Fix tock

--- test/tock.rb
+++ test/tock.rb
@@ -8,7 +8,7 @@ p.enable
 
 while true do
   if t.enabled?
-    t.fire_tracepoint([StaticTracing.nsec, "Hello world"])
+    t.fire(StaticTracing.nsec, "Hello world")
     puts "Probe fired!"
   else
     puts "Not enabled"
