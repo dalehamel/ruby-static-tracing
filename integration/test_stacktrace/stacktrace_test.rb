b19d67dbd5038bfc16bdd0b78de9865932c97340
Gustavo Caso <gustavo.caso@shopify.com>
2019-04-06 10:24:57 -0400

    add integration tests for stacktracer

---
+++ integration/test_stacktrace/stacktrace_test.rb
@@ -0,0 +1,20 @@
+require 'integration_helper'
+
+# FIXME can any of this be generalized / should the convention be encoded?
+class StacktraceTest < IntegrationTestCase
+  def test_stacktrace
+    target = command('bundle exec ruby stacktrace.rb', wait: 1)
+    # Enable probing
+    target.prof(1)
+
+    tracer = command("bpftrace stacktrace.bt -p #{target.pid}", wait: 5)
+
+    # Signal the target to trigger probe firing
+    target.usr2(1)
+
+    # Signal bpftrace to exit, flushing output
+    tracer.interrupt(1)
+
+    assert_tracer_output(tracer.output, read_probe_file('stacktrace.out'))
+  end
+end
