53f2472222f7e8a61828b595a60c21a6054f6ce7
Dale Hamel <dale.hamel@srvthe.net>
2019-04-22 16:18:14 -0400

    Test to ensure tracepoint toggling is working

--- test/ruby_static_tracing_test.rb
+++ test/ruby_static_tracing_test.rb
@@ -7,6 +7,11 @@ class DummyClass
 end
 
 class RubyStaticTracingTest < MiniTest::Test
+  class Example
+    def noop; end
+    StaticTracing::Tracer::Latency.register(self, :noop)
+  end
+
   def test_nsec_returns_monotonic_time_in_nanoseconds
     Process
       .expects(:clock_gettime)
@@ -18,9 +23,12 @@ class RubyStaticTracingTest < MiniTest::Test
   def test_toggle_tracing
     StaticTracing.enable!
     assert StaticTracing.enabled?
+    assert StaticTracing::Provider.fetch('ruby_static_tracing_test_example').enabled?
     StaticTracing.toggle_tracing!
     refute StaticTracing.enabled?
+    refute StaticTracing::Provider.fetch('ruby_static_tracing_test_example').enabled?
     StaticTracing.toggle_tracing!
     assert StaticTracing.enabled?
+    assert StaticTracing::Provider.fetch('ruby_static_tracing_test_example').enabled?
   end
 end
