cf9af1d06f8efe03a12ef8605fe21691fe6dae47
Dale Hamel <dale.hamel@srvthe.net>
2019-04-22 04:22:54 -0400

    WIP

--- lib/ruby-static-tracing/tracer/latency.rb
+++ lib/ruby-static-tracing/tracer/latency.rb
@@ -9,7 +9,7 @@ module StaticTracing
         duration = StaticTracing.nsec - start_time
         method_name = __method__.to_s
         provider = Tracer::Helpers.underscore(self.class.name)
-        # FIXME benchmark this, we may need to cache the provider instance on the object
+        # FIXME: benchmark this, we may need to cache the provider instance on the object
         # This lookup is a bit of a hack
         t = Provider.fetch(provider).tracepoints[method_name]
         t.fire(method_name, duration) if t
